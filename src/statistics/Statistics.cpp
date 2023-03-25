4df04c0a8ca1841cf87cfbf33f2ba39d72d295c8
Pascal Spörri <psp@zurich.ibm.com>
2023-03-25 12:47:33 +0100

    Statistics: Fix histogram.
    Signed-off-by: Pascal Spörri <psp@zurich.ibm.com>

--- src/statistics/Statistics.cpp
+++ src/statistics/Statistics.cpp
@@ -72,20 +72,8 @@ std::shared_ptr<StatisticsHistogram>
 Statistics::createNanoSecondHistogram(const std::string &label) {
   constexpr size_t ONE_MIN = 60000000000; // Minute
   const static auto NANOSECOND_HISTOGRAM = std::vector<size_t>{
-      010,          020,          030,          040,          050,          // 50ns
-      100,          200,          300,          400,          500,          // 0.5us
-      1000,         2000,         3000,         4000,         5000,         // 5us
-      10000,        20000,        30000,        40000,        50000,        // 50us
-      100000,       200000,       300000,       400000,       500000,       // 500us
-      1000000,      2000000,      3000000,      4000000,      5000000,      // 5ms
-      10000000,     20000000,     30000000,     40000000,     50000000,     // 50ms
-      100000000,    200000000,    300000000,    400000000,    500000000,    // 500ms
-      1000000000,   2000000000,   3000000000,   4000000000,   5000000000,   // 5s
-      10000000000,  20000000000,  30000000000,  40000000000,  50000000000,  // 50s
-      ONE_MIN * 1,  ONE_MIN * 2,  ONE_MIN * 3,  ONE_MIN * 4,  ONE_MIN * 5,  // 5min,
-      ONE_MIN * 10, ONE_MIN * 20, ONE_MIN * 30, ONE_MIN * 40, ONE_MIN * 50, // 50min
-      ONE_MIN * 60                                                          // 1h
-  };
+      1,        10,        100,        1000,        10000,       100000,       1000000,
+      10000000, 100000000, 1000000000, 10000000000, ONE_MIN * 1, ONE_MIN * 10, ONE_MIN * 60};
 
   return createHistogram(label, NANOSECOND_HISTOGRAM);
 }
