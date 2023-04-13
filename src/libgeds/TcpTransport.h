db146be72190fea7e7a54d3997afb7f67dc209dc
Pascal Spörri <psp@zurich.ibm.com>
2023-04-13 09:02:10 +0200

    TcpTransport: Improve handling of crashed endpoints.
    Signed-off-by: Pascal Spörri <psp@zurich.ibm.com>

--- src/libgeds/TcpTransport.h
+++ src/libgeds/TcpTransport.h
@@ -29,6 +29,7 @@
 #include "ConcurrentMap.h"
 #include "ConcurrentQueue.h"
 #include "FileTransferProtocol.h"
+#include "RWConcurrentObjectAdaptor.h"
 #include "Statistics.h"
 #include "StatisticsGauge.h"
 
@@ -142,7 +143,7 @@ using epoll_epid_t = union EpollEpId {
 };
 
 class TcpTransport;
-class TcpPeer : public std::enable_shared_from_this<TcpPeer> {
+class TcpPeer : public std::enable_shared_from_this<TcpPeer>, utility::RWConcurrentObjectAdaptor {
 
 private:
   friend class TcpTransport;
@@ -161,7 +162,6 @@ private:
       Statistics::createGauge("GEDS: TcpTransport recvQueue length");
 
   std::map<int, std::shared_ptr<TcpEndpoint>> endpoints;
-  mutable std::shared_mutex epMux;
 
   bool processEndpointSend(std::shared_ptr<TcpEndpoint> tep);
   bool processEndpointRecv(int sock);
@@ -181,9 +181,8 @@ public:
   sendRpcRequest(uint64_t dest, std::string name, size_t src_off, size_t len);
   int sendRpcReply(uint64_t reqId, int in_fd, uint64_t start, size_t len, int status);
   void addEndpoint(std::shared_ptr<TcpEndpoint> tep) {
-    epMux.lock();
+    auto lock = getWriteLock();
     endpoints.emplace(tep->sock, tep);
-    epMux.unlock();
   };
   TcpPeer(std::string name, std::shared_ptr<GEDS> geds, TcpTransport &tcpTransport)
       : Id(SStringHash(name)), _geds(std::move(geds)), _tcpTransport(tcpTransport),
