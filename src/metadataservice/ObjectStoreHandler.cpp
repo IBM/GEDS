c1628e1d205186697b7c46be12318a7f61211d4c
Pascal Spörri <psp@zurich.ibm.com>
2023-04-25 09:26:28 +0200

    Metadata Server: ObjectStoreHandler fix insertion into object store.
    Signed-off-by: Pascal Spörri <psp@zurich.ibm.com>

--- src/metadataservice/ObjectStoreHandler.cpp
+++ src/metadataservice/ObjectStoreHandler.cpp
@@ -12,11 +12,12 @@ ObjectStoreHandler::insertConfig(std::shared_ptr<const geds::ObjectStoreConfig> 
   auto lock = getWriteLock();
   auto key = config->bucket;
   auto [it, success] = _map.insert(std::make_pair(key, config));
-  if (!success && *(it->second) != *config) {
+  if (!success) {
     return absl::AlreadyExistsError("ObjectStore config for bucket already exists: '" + key + "'.");
   }
   return absl::OkStatus();
 }
+
 std::vector<std::shared_ptr<const geds::ObjectStoreConfig>>
 ObjectStoreHandler::listConfigs() const {
   auto lock = getReadLock();
