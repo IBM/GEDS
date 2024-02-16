b128f9c6c0b3025f763a33fcefcba032ee6271be
Pascal Spörri <psp@zurich.ibm.com>
2024-02-16 16:16:04 +0100

    Fix Docker Build Script.
    Signed-off-by: Pascal Spörri <psp@zurich.ibm.com>

--- build-docker.sh
+++ build-docker.sh
@@ -19,8 +19,11 @@ git diff-files --quiet || GIT_REVISION="${GIT_REVISION}-dirty"
 GEDS_VERSION=${TRAVIS_TAG:-$(git describe --tags --match "v*" --dirty)}
 [[ "$GEDS_VERSION" =~ ^v ]] && GEDS_VERSION=$(echo $GEDS_VERSION | cut -c 2-)
 
-IMAGE_NAME=geds-dev_${DOCKER_BUILD_TARGET}:${GEDS_DOCKER_VERSION}
-IMAGE=us.icr.io/zrlio/${IMAGE_NAME}
+DOCKER_IMAGE_PREFIX=${DOCKER_IMAGE_PREFIX:-"zac32.zurich.ibm.com/zrlio/"}
+IMAGE_PREFIX=geds-dev_${DOCKER_BUILD_TARGET}
+IMAGE_NAME=${IMAGE_PREFIX}:${GEDS_DOCKER_VERSION}
+echo $IMAGE_NAME
+IMAGE=${DOCKER_IMAGE_PREFIX}${IMAGE_PREFIX}
 
 BASE_BUILD_IMAGE="geds_build-${DOCKER_BUILD_TARGET}:${GEDS_DOCKER_VERSION}"
 docker build -t ${BASE_BUILD_IMAGE} \
@@ -34,7 +37,7 @@ docker build -t ${GRPC_DOCKER_IMAGE} \
     --build-arg AWS_SDK_VERSION=${AWS_SDK_VERSION} \
     --build-arg BOOST_VERSION=${BOOST_VERSION} \
     --build-arg BOOST_VERSION_=${BOOST_VERSION//./_} \
-    --build-arg CMAKE_BUILD_PARALLEL_LEVEL=$(($(nproc) + 1)) \
+    --build-arg CMAKE_BUILD_PARALLEL_LEVEL=$(( $(nproc) + 1)) \
     -f docker/Dockerfile-dependencies .
 
 docker build -t geds-build/${DOCKER_BUILD_TARGET}:${GIT_REVISION} \
@@ -42,14 +45,23 @@ docker build -t geds-build/${DOCKER_BUILD_TARGET}:${GIT_REVISION} \
     --build-arg DOCKER_BUILD_TARGET=${DOCKER_BUILD_TARGET} \
     --build-arg GEDS_VERSION=${GEDS_VERSION} \
     --build-arg DOCKER_BUILD_TARGET=${DOCKER_BUILD_TARGET} \
-    --build-arg CMAKE_BUILD_PARALLEL_LEVEL=$(($(nproc) + 1)) \
+    --build-arg CMAKE_BUILD_PARALLEL_LEVEL=$(( $(nproc) + 1)) \
     -f docker/Dockerfile-build .
 
-docker build -t ${IMAGE_NAME}:${GIT_REVISION} \
+docker build -t ${IMAGE_NAME} \
     --build-arg GIT_REVISION=${GIT_REVISION} \
     -f docker/Dockerfile_${DOCKER_BUILD_TARGET} .
 
-docker tag ${IMAGE_NAME}:${GIT_REVISION} $IMAGE:latest
-docker tag ${IMAGE_NAME}:${GIT_REVISION} $IMAGE:${GEDS_VERSION}
+echo "Built ${IMAGE_NAME}"
+
+docker tag ${IMAGE_NAME} $IMAGE:latest
+docker tag ${IMAGE_NAME} $IMAGE:${GEDS_VERSION}
+
+echo "Built ${IMAGE}:latest"
+echo "Built ${IMAGE}:${GEDS_VERSION}"
+
 docker push $IMAGE:latest
 docker push $IMAGE:${GEDS_VERSION}
+
+echo "Pushed ${IMAGE}:latest" 
+echo "Pushed ${IMAGE}:${GEDS_VERSION}"
