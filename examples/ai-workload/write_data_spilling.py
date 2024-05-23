588a8ac7b8ca1ee1315c5d0c53dcbd1da5aca9d6
Pascal Spörri <psp@zurich.ibm.com>
2024-05-23 14:23:09 +0200

    Add AI Training Examples.
    Signed-off-by: Pascal Spörri <psp@zurich.ibm.com>

---
+++ examples/ai-workload/write_data_spilling.py
@@ -0,0 +1,66 @@
+import os
+import sys
+import time
+import io
+import numpy as np
+from threading import Thread
+
+
+from smart_open import open
+import geds_smart_open
+from geds_smart_open import GEDS
+
+bucket = 'geds-test'
+
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_ENDPOINT_URL = os.getenv('AWS_ENDPOINT_URL')
+geds_smart_open.register_object_store(bucket, AWS_ENDPOINT_URL, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+
+MAX_THREADS = 16
+MAX_SIZE = 28 # 256 MB
+MAX_SIZE = 33 # 8GB
+
+output_csv = open('file://write_data.csv', 'w')
+output_csv.write("Payload Size,Thread Count,Throughput\n")
+
+
+spill_csv = open('file://write_data_spill.csv', 'w')
+spill_csv.write("Payload Size,Object Count,Throughput\n")
+
+def write_file(tid, nthreads, buffer, size):
+    path = f'geds://{bucket}/benchmark/{nthreads}_{tid}_{size}'
+    with open(path, 'wb') as f:
+        f.write(buffer)
+        f.flush()
+        f.close()
+
+for t in range(0, MAX_THREADS):
+    num_threads = t + 1
+    if num_threads > 1 and num_threads % 2 == 1:
+        continue
+    for s in range(1, MAX_SIZE+1):
+        size = 2**s
+#        if num_threads == 16 and s == MAX_SIZE:
+#            continue
+        buffers = [np.random.randint(0, high=255, size=size, dtype=np.uint8) for i in range(0, num_threads)]
+        threads = [Thread(target=write_file, args=[i, num_threads, buffers[i], size]) for i in range(0, num_threads)]
+        start_time = time.time_ns()
+        [t.start() for t in threads]
+        [t.join() for t in threads]
+        duration = (time.time_ns() - start_time) / (1000**3)
+        rate = float(num_threads) * size / duration / (1024**2)
+        print(f"{size}, {num_threads}: {rate} MB/s")
+        output_csv.write(f"{size},{num_threads},{rate}\n")
+        output_csv.flush()
+        start_time = time.time_ns()
+        geds_smart_open.relocate()
+        duration = (time.time_ns() - start_time) / (1000**3)
+        rel_size = size * num_threads
+        rate = float(rel_size) / duration / (1024**2)
+        print(f"Relocation {rel_size}, {num_threads}: {rate}")
+        spill_csv.write(f"{rel_size},{num_threads},{rate}\n")
+        spill_csv.flush()
+spill_csv.close()
+output_csv.close()
+
